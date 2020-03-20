dd68f01cda038f543499bb8080402b3e4f5d1179
lt5227 <995362096@qq.com>
2020-03-20 15:18:40 +0800

    Maven 打包例程代码

---
+++ MavenStudy/src/main/java/com/stackstone/study/maven/HelloMaven.java
@@ -0,0 +1,26 @@
+package com.stackstone.study.maven;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+public class HelloMaven {
+    public static void main(String[] args) {
+        var str = "Hello Maven";
+        System.out.println(str);
+        InputStream inputStream = HelloMaven.class.getClassLoader().getResourceAsStream("demo.yml");
+        Yaml yaml = new Yaml();
+        for (Object o : yaml.loadAll(inputStream)) {
+            Map map = (Map) o;
+            System.out.println(map);
+        }
+        try {
+            assert inputStream != null;
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
