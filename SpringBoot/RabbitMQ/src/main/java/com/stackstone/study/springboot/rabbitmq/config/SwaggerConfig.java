dc8e386de09a683eaed845a5f554cf340e6d5463
lile <lilei@oristand.com>
2020-10-19 21:03:26 +0800

    spring boot rabbitmq

--- SpringBoot/RabbitMQ/src/main/java/com/stackstone/study/springboot/rabbitmq/config/SwaggerConfig.java
+++ SpringBoot/RabbitMQ/src/main/java/com/stackstone/study/springboot/rabbitmq/config/SwaggerConfig.java
@@ -1,7 +1,17 @@
 package com.stackstone.study.springboot.rabbitmq.config;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import com.stackstone.study.springboot.rabbitmq.config.properties.SwaggerProperties;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -18,5 +28,43 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableKnife4j
 public class SwaggerConfig {
+    private final SwaggerProperties swaggerProperties;
 
+    public SwaggerConfig(SwaggerProperties swaggerProperties) {
+        this.swaggerProperties = swaggerProperties;
+    }
+
+    /**
+     * 创建API
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                // 用来创建该API的基本信息，展示在文档的页面中（自定义展示的信息）
+                .apiInfo(apiInfo())
+                // 分组名称
+                .groupName(swaggerProperties.getName())
+                // 设置哪些接口暴露给Swagger展示
+                .select()
+                // 扫描所有有注解的api，用这种方式更灵活
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 添加摘要信息
+     */
+    private ApiInfo apiInfo() {
+        // 用ApiInfoBuilder进行定制
+        return new ApiInfoBuilder()
+                // 设置标题
+                .title(swaggerProperties.getTitle())
+                .contact(new Contact(swaggerProperties.getAuthor(), swaggerProperties.getUrl(), swaggerProperties.getEmail()))
+                // 描述
+                .description(swaggerProperties.getDescription())
+                // 版本
+                .version(swaggerProperties.getVersion())
+                .build();
+    }
 }
